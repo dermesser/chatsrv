e1670658ccaeac91713dbd5d40c31f7bbef87894
lbo <lbo@spheniscida.de>
2012-09-21 21:33:03 +0200

    Removed unnecessary utility functions

--- chat_util.erl
+++ chat_util.erl
@@ -1,21 +1,4 @@
 -module(chat_util).
--export([find_mesg_limit/1,greatest/1]).
+-export([]).
 
-%% Find the begin of the message in a Channel+0+Message binary
-find_mesg_limit(Message) -> find_mesg_limit(Message,1).
-
-find_mesg_limit(<<>>,N) -> N;
-find_mesg_limit(Message,N) ->
-	case split_binary(Message,1) of
-		{<<0>>,_Rest} -> N;
-		{_OtherChar,Rest} -> find_mesg_limit(Rest,N+1)
-	end.
-
-greatest(List) -> greatest(List,0).
-
-greatest([],Yet) -> Yet;
-greatest([H|T],Yet) ->
-	if
-		H > Yet -> greatest(T,H);
-		true -> greatest(T,Yet)
-	end.
+%% Eventual utility functions necessary for chatsrv
